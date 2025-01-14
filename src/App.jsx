cd81461d8513187fe74fad52f7814c831b7ae01f
Albert Osei Frimpong <alotab@yahoo.com>
2025-01-14 13:14:08 -0800

    Update components

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,26 @@ import './App.css'
 import Chat from './componemts/chat/Chat'
 import Detail from './componemts/detail/Detail'
 import List from './componemts/list/List'
+import Login from './componemts/login/Login'
+import Notification from './componemts/notification/Notification'
 
-function App() {
+
+const App = ()=> {
   
-
+  const user = true;
   return (
     <>
       <div className='container'>
-        <List />
-        <Chat />
-        <Detail />
+        { user ? (
+            <>
+                <List />
+                <Chat />
+                <Detail />
+            </>
+           
+          ) : (<Login />)
+        }
+        <Notification />
       </div>
     </>
   )
